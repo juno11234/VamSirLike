14166aac37871c965a809c6321e45e36f0099c7a
juno11234 <cozam2@naver.com>
2026-03-03 21:33:02 +0900

    이동 카메라, 맵 완성

--- Assets/2_Scripts/Player/PlayerController.cs
+++ Assets/2_Scripts/Player/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private float _moveSpeed;
     private bool _isInitialized = false;
 
+    [Header("Map Boundaries")]
+    public Vector2 minBounds = new Vector2(-10f, -10f); // 좌하단 끝 좌표
+    public Vector2 maxBounds = new Vector2(10f, 10f);   // 우상단 끝 좌표
     public void Initialize(PlayerStat stat)
     {
         _moveSpeed = stat.baseSpeed;
@@ -32,6 +35,9 @@ public class PlayerController : MonoBehaviour
     {
         if (_isInitialized == false) return;
         Vector3 moveDir = new Vector3(_moveInput.x, _moveInput.y, 0f).normalized;
-        transform.position += moveDir * (_moveSpeed * Time.deltaTime);
+        Vector3 nextPosition = transform.position + moveDir * (_moveSpeed * Time.deltaTime);
+        nextPosition.x = Mathf.Clamp(nextPosition.x, minBounds.x, maxBounds.x);
+        nextPosition.y = Mathf.Clamp(nextPosition.y, minBounds.y, maxBounds.y);
+        transform.position = nextPosition;
     }
 }
