779d48b96ebb1c587b7c581dd793eb6f98f8f87f
juno11234 <cozam2@naver.com>
2026-03-04 17:26:30 +0900

    플레이어 좌우반전 및 깃이그노어 레이아웃 추가

--- Assets/2_Scripts/Player/PlayerController.cs
+++ Assets/2_Scripts/Player/PlayerController.cs
@@ -10,9 +10,9 @@ public class PlayerController : MonoBehaviour
     private float _moveSpeed;
     private bool _isInitialized = false;
 
-    [Header("Map Boundaries")]
-    public Vector2 minBounds = new Vector2(-10f, -10f); // 좌하단 끝 좌표
-    public Vector2 maxBounds = new Vector2(10f, 10f);   // 우상단 끝 좌표
+    [Header("Map Boundaries")] public Vector2 minBounds = new Vector2(-10f, -10f); // 좌하단 끝 좌표
+    public Vector2 maxBounds = new Vector2(10f, 10f); // 우상단 끝 좌표
+
     public void Initialize(PlayerStat stat)
     {
         _moveSpeed = stat.baseSpeed;
@@ -35,6 +35,12 @@ public class PlayerController : MonoBehaviour
     {
         if (_isInitialized == false) return;
         Vector3 moveDir = new Vector3(_moveInput.x, _moveInput.y, 0f).normalized;
+        
+        if (moveDir.x != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(moveDir.x), 1f, 1f);
+        }
+
         Vector3 nextPosition = transform.position + moveDir * (_moveSpeed * Time.deltaTime);
         nextPosition.x = Mathf.Clamp(nextPosition.x, minBounds.x, maxBounds.x);
         nextPosition.y = Mathf.Clamp(nextPosition.y, minBounds.y, maxBounds.y);
