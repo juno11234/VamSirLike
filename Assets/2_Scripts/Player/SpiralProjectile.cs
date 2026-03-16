f5e050b6762d561a2409b4446d4ccd2d007b54f2
juno11234 <cozam2@naver.com>
2026-03-16 13:25:33 +0900

    도끼 스킬 판정

--- Assets/2_Scripts/Player/SpiralProjectile.cs
+++ Assets/2_Scripts/Player/SpiralProjectile.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class SpiralProjectile : MonoBehaviour
 {
- private float _angle;
+    [SerializeField] private LayerMask enemyLayer;
+    
+    private float _angle;
     private float _radius;
     private Transform _center; // 회전의 중심 (플레이어)
     private float _angleSpeed;
@@ -14,20 +16,30 @@ public class SpiralProjectile : MonoBehaviour
     private IFighter _sender;
     private Action<SpiralProjectile> _onRelease;
 
+    private Collider2D[] _results = new Collider2D[10];
+    private ContactFilter2D _filter;
+    private float _hitRadius = 0.5f; // 투사체 크기
+
     // 풀에서 꺼낼 때마다 호출될 초기화 함수
-    public void Init(CombatSystem combatSystem, IFighter sender, Transform center, float startAngle, float angleSpeed, float expandSpeed, float damage, Action<SpiralProjectile> onRelease)
+    public void Init(CombatSystem combatSystem, IFighter sender, Transform center, float startAngle, float angleSpeed,
+        float expandSpeed, float damage, Action<SpiralProjectile> onRelease)
     {
         _combatSystem = combatSystem;
         _sender = sender;
         _center = center;
-        
+
         _angle = startAngle;
         _angleSpeed = angleSpeed;
         _expandSpeed = expandSpeed;
         _damage = damage;
-        
+
         _radius = 0f; // 중심(0)에서부터 출발
         _onRelease = onRelease;
+
+        _filter = new ContactFilter2D();
+        _filter.SetLayerMask(enemyLayer); // 몬스터 레이어만!
+        _filter.useLayerMask = true;
+        _filter.useTriggers = true;
     }
 
     private void Update()
@@ -46,35 +58,30 @@ public class SpiralProjectile : MonoBehaviour
         // --- [추가된 회전 로직] ---
         // _angle은 라디안(Radian) 값이므로 우리가 아는 도(Degree) 단위로 변환해 줍니다.
         float angleDeg = _angle * Mathf.Rad2Deg;
-        
-        transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90f); 
-        // -----------------------
 
-        // 4. 화면 밖으로 충분히 멀어지면 풀로 반납
-        if (_radius > 15f)
-        {
-            _onRelease?.Invoke(this);
-        }
-    }
+        transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90f);
 
-    // 적과 충돌 시 데미지 처리
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // Tag가 "Enemy"인지 확인 (프로젝트 설정에 맞게 변경하세요)
-        if (other.CompareTag("Enemy"))
+        int hitCount = Physics2D.OverlapCircle(transform.position, _hitRadius, _filter, _results);
+        for (int i = 0; i < hitCount; i++)
         {
-            IFighter receiver = other.GetComponent<IFighter>();
-            if (receiver != null)
+            IFighter targetMonster = _combatSystem.GetMonster(_results[i]);
+            if (targetMonster != null)
             {
                 InGameEvent evt = new InGameEvent
                 {
                     Type = InGameEvent.EventType.Combat,
                     Sender = _sender,
-                    Receiver = receiver,
+                    Receiver = targetMonster,
                     Amount = _damage
                 };
                 _combatSystem.AddInGameEvent(evt);
             }
+        }
+
+        // 4. 화면 밖으로 충분히 멀어지면 풀로 반납
+        if (_radius > 15f)
+        {
+            _onRelease?.Invoke(this);
         }
     }
 }
